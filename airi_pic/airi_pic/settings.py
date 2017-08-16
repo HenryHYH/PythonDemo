a24870ffcd627b3ae6ee810c7430f88603eef974
Henry <henry.hyh@gmail.com>
2017-08-16 11:20:07 +0800

    PythonDemo，scrapy图片下载，调整配置，禁用robots规则

--- airi_pic/airi_pic/settings.py
+++ airi_pic/airi_pic/settings.py
@@ -18,7 +18,7 @@ NEWSPIDER_MODULE = 'airi_pic.spiders'
 #USER_AGENT = 'airi_pic (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
