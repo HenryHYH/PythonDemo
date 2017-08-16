d8e32ce29e6f6886b72b04e19d1d143834238a97
Henry <henry.hyh@gmail.com>
2017-08-16 10:40:09 +0800

    PythonDemo，添加scrapy示例

--- HelloScrapy/HelloScrapy/spiders/quotes_spider2.py
+++ HelloScrapy/HelloScrapy/spiders/quotes_spider2.py
@@ -7,18 +7,13 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     """quotes"""
 
-    name = 'quotes'
+    name = 'quotes2'
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
         'http://quotes.toscrape.com/page/2/'
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as file:
-            file.write(response.body)
-
         for quote in response.css("div.quote"):
             yield {
                 'text': quote.css("span.text::text").extract_first(),
